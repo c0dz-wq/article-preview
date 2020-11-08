7a831640783d430dbe66ef2df965bdc446890fea
Zeljko Stojanov <4daweb@gmail.com>
2020-11-08 17:06:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.social__container'); 
+const shareBtn = document.querySelector('.share__btn');
+const previewCard = document.querySelector('.preview__card');
+ 
+
+function toggleActive() {
+	shareBtn.classList.toggle('share__btn--active');
+	container.classList.toggle('social--active'); 
+	previewCard.classList.toggle('preview__card--active');
+}
+
+shareBtn.addEventListener('click', () => { toggleActive(); });
